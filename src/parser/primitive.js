e0b1147815cb90f09dd2f613f531c4dda528e868
Robin Heggelund Hansen <git@heghan.org>
2023-12-10 15:26:29 +0100

    Add a few more parsers.

--- src/parser/primitive.js
+++ src/parser/primitive.js
@@ -3,3 +3,33 @@ export function char(c) {
     return str[0] === c;
   };
 }
+
+export function digit(str) {
+  switch (str[0]) {
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      return true;
+    default:
+      return false;
+  }
+}
+
+export function oneOf(...parsers) {
+  return (str) => {
+    for (const parser of parsers) {
+      if (parser(str)) {
+        return true;
+      }
+    }
+
+    return false;
+  };
+}
--- tests/parser/primitive.test.js
+++ tests/parser/primitive.test.js
@@ -11,4 +11,42 @@ describe("Parser primitives", () => {
       assert.ok(!parse.char("b")("cars"));
     });
   });
+
+  describe("digit", () => {
+    it("parses 0-9", () => {
+      assert.ok(parse.digit("0"));
+      assert.ok(parse.digit("1"));
+      assert.ok(parse.digit("2"));
+      assert.ok(parse.digit("3"));
+      assert.ok(parse.digit("4"));
+      assert.ok(parse.digit("5"));
+      assert.ok(parse.digit("6"));
+      assert.ok(parse.digit("7"));
+      assert.ok(parse.digit("8"));
+      assert.ok(parse.digit("9"));
+    });
+
+    it("sanity check: fails if passed string starting with different character", () => {
+      assert.ok(!parse.digit("arr"));
+    });
+  });
+
+  describe("oneOf", () => {
+    it("succeeds if any given parser succeeds", () => {
+      const parser = parse.oneOf(parse.digit, parse.char("e"));
+
+      assert.ok(parser("1"));
+      assert.ok(parser("e"));
+    });
+
+    it("fails if no given parser succeeds", () => {
+      const parser = parse.oneOf(
+        parse.char("a"),
+        parse.char("b"),
+        parse.char("c"),
+      );
+
+      assert.ok(!parser("delta"));
+    });
+  });
 });
