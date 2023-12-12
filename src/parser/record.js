cf000033c9d25b774a64ff16bdb4947f2b3f74c0
Robin Heggelund Hansen <git@heghan.org>
2023-12-12 13:33:54 +0100

    Fix formatting.

--- src/parser/record.js
+++ src/parser/record.js
@@ -2,12 +2,7 @@ import * as parse from "./primitive.js";
 import * as atom from "./atom.js";
 
 export const emptyRecord = parse
-  .sequence(
-    parse.char("["),
-
-    atom.whitespace,
-    parse.char("]"),
-  )
+  .sequence(parse.char("["), atom.whitespace, parse.char("]"))
   .map((_) => ({}));
 
 export const nonEmptyRecord = parse.sequence(parse.char("["), parse.char("]"));
