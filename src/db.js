7cb71869cbe8283f02f113cc2a23356fff5f3fb3
Robin Heggelund Hansen <git@heghan.org>
2023-12-19 09:14:49 +0100

    Add semicolons.

--- src/db.js
+++ src/db.js
@@ -1,11 +1,11 @@
-import sqlite from "sqlite3"
+import sqlite from "sqlite3";
 
 export function setup(connectionString, cb) {
-  const db = new sqlite.Database(connectionString)
+  const db = new sqlite.Database(connectionString);
 
   // TODO: split in permanent and run-once pragmas
-  db.run("PRAGMA encoding = 'UTF-8'")
-  db.run("PRAGMA foreign_keys = ON")
+  db.run("PRAGMA encoding = 'UTF-8'");
+  db.run("PRAGMA foreign_keys = ON");
 
-  cb(db)
+  cb(db);
 }
