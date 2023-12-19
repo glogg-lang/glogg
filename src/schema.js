e5e00d5b7d61aa5098ab6365601089665de1a60d
Robin Heggelund Hansen <git@heghan.org>
2023-12-19 21:07:53 +0100

    Add two new tables for representing clauses

--- src/schema.js
+++ src/schema.js
@@ -15,5 +15,20 @@ export const migrations = [
       id INTEGER PRIMARY KEY,
       query_id INTEGER NOT NULL REFERENCES query(id) ON DELETE CASCADE
     ) STRICT`,
+    `CREATE TABLE clause (
+      id INTEGER PRIMARY KEY,
+      search_id INTEGER NOT NULL REFERENCES search(id) ON DELETE CASCADE,
+      bind_id INTEGER NOT NULL REFERENCES bind(id) ON DELETE CASCADE,
+      commit_id INTEGER NOT NULL REFERENCES 'commit'(id) ON DELETE CASCADE,
+      alias TEXT,
+      'order' INTEGER NOT NULL
+    ) STRICT`,
+    `CREATE TABLE 'constraint' (
+      id INTEGER PRIMARY KEY,
+      clause_id INTEGER NOT NULL REFERENCES clause(id) ON DELETE CASCADE,
+      column TEXT NOT NULL,
+      value ANY NOT NULL,
+      operator TEXT NOT NULL
+    ) STRICT`,
   ],
 ];
