87caad1cff1bb3bd04128d14aafdab34fe509259
Robin Heggelund Hansen <git@heghan.org>
2023-12-25 14:55:03 +0100

    Parse conditional queries.

--- src/parser/query.js
+++ src/parser/query.js
@@ -2,8 +2,9 @@ import * as parse from "./primitive.js";
 import * as atom from "./atom.js";
 import * as rec from "./record.js";
 
-export const query = parse
+const unconditionalCommit = parse
   .sequence(
+    atom.whitespace,
     parse.word("commit:"),
     atom.whitespace,
     parse.nOrMore(1, rec.nonEmptyRecord).keep(),
@@ -13,3 +14,32 @@ export const query = parse
     bind: [],
     commit: records,
   }));
+
+const unconditionalBind = parse
+  .sequence(
+    atom.whitespace,
+    parse.word("bind:"),
+    atom.whitespace,
+    parse.nOrMore(1, rec.nonEmptyRecord).keep(),
+  )
+  .mapKeeps(([records]) => ({
+    search: [],
+    bind: records,
+    commit: [],
+  }));
+
+const conditionalQuery = parse
+  .sequence(
+    atom.whitespace,
+    parse.word("search:"),
+    atom.whitespace,
+    parse.nOrMore(1, rec.nonEmptyRecord).keep(),
+    parse.oneOf(unconditionalCommit, unconditionalBind).keep(),
+  )
+  .mapKeeps(([searchRecords, modifierRecords]) => ({
+    search: searchRecords,
+    bind: modifierRecords.bind,
+    commit: modifierRecords.commit,
+  }));
+
+export const query = parse.oneOf(unconditionalCommit, conditionalQuery);
--- test/parser/query.test.js
+++ test/parser/query.test.js
@@ -1,8 +1,9 @@
 import * as parse from "#src/parser/query";
+import * as atom from "#src/parser/atom";
 import * as assert from "node:assert";
 
 describe("Query parsing", () => {
-  describe("Simple commit", () => {
+  describe("Commit", () => {
     it("describing an unconditional fact", () => {
       const result = parse.query.run(
         ["commit:", '  [#person name: "Robin" role: "developer"]'].join("\n"),
@@ -38,6 +39,64 @@ describe("Query parsing", () => {
         { tag: "person", name: "Robin", role: "developer" },
         { tag: "person", name: "Nibor", role: "team lead", cats: 3 },
       ]);
+    });
+
+    it("Commits can be prefixed with a search block", () => {
+      const result = parse.query.run(
+        [
+          "search:",
+          "  [#person name: name pets: n]",
+          "",
+          "commit:",
+          "  [#cat-person name: name]",
+        ].join("\n"),
+      );
+
+      assert.ok(result.success);
+
+      const query = result.value;
+
+      assert.deepStrictEqual(query.search, [
+        { tag: "person", name: new atom.Var("name"), pets: new atom.Var("n") },
+      ]);
+      assert.deepStrictEqual(query.bind, []);
+      assert.deepStrictEqual(query.commit, [
+        { tag: "cat-person", name: new atom.Var("name") },
+      ]);
+    });
+  });
+
+  describe("Bind", () => {
+    it("Bind must always be prefixed with a search block", () => {
+      const result = parse.query.run(
+        [
+          "search:",
+          "  [#person name: name pets: n]",
+          "",
+          "bind:",
+          "  [#cat-person name: name]",
+        ].join("\n"),
+      );
+
+      assert.ok(result.success);
+
+      const query = result.value;
+
+      assert.deepStrictEqual(query.search, [
+        { tag: "person", name: new atom.Var("name"), pets: new atom.Var("n") },
+      ]);
+      assert.deepStrictEqual(query.bind, [
+        { tag: "cat-person", name: new atom.Var("name") },
+      ]);
+      assert.deepStrictEqual(query.commit, []);
+    });
+
+    it("Bind without search will fail (it would imply commit)", () => {
+      const result = parse.query.run(
+        ["bind:", "  [#cat-person name: name]"].join("\n"),
+      );
+
+      assert.strictEqual(false, result.success);
     });
   });
 });
