debda3d17cca9e2d060a82cc35057c185260cd97
Robin Heggelund Hansen <git@heghan.org>
2023-12-25 18:05:28 +0100

    Remove whitespace before checking AST code for equality

--- test/ast.test.js
+++ test/ast.test.js
@@ -2,24 +2,24 @@ import * as ast from "#src/ast";
 import * as db from "#src/db";
 import * as assert from "node:assert";
 
-describe("AST", () => {
-  describe("save", () => {
-    it("Parsed AST can be stored into a database", async () => {
-      const store = await db.setup(":memory:");
+let store = null;
 
-      await ast.save(
-        store,
-        'commit: [#person name: "Robin" pets: 2 role: role]',
-      );
+describe("AST - Save and Load", () => {
+  beforeEach(async () => {
+    store = await db.setup(":memory:");
+  });
+  it("Unconditional commit", async () => {
+    await ast.save(store, 'commit: [#person name: "Robin" pets: 2 role: role]');
 
-      const extracted = await ast.load(store);
+    const extracted = await ast.load(store);
 
-      assert.equal(
-        ["commit:", '  [ #person name: "Robin" pets: 2 role: role ]'].join(
-          "\n",
-        ),
-        extracted,
-      );
-    });
+    assertNoWhitespace(
+      'commit: [ #person name: "Robin" pets: 2 role: role ]',
+      extracted,
+    );
   });
 });
+
+function assertNoWhitespace(expected, actual) {
+  assert.strictEqual(expected.replace(/\s/g, ""), actual.replace(/\s/g, ""));
+}
